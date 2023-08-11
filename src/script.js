071625118751b6f8aeba97d4ecd9695ace0abfff
Zachary Morgan <zsmorgan21@gmail.com>
2023-08-10 21:19:07 -0400

    now itll search iframes too. ESPN+ now supported!

--- src/script.js
+++ src/script.js
@@ -1,25 +1,23 @@
-// Copyright 2018 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 function findLargestPlayingVideo() {
-  const videos = Array.from(document.querySelectorAll('video'))
-    .filter(video => video.readyState != 0)
-    .filter(video => video.disablePictureInPicture == false)
+  let videos = [];
+
+  videos.push(...document.querySelectorAll("video"));
+
+  document
+    .querySelectorAll("iframe")
+    .forEach((item) =>
+      videos.push(...item.contentWindow.document.querySelectorAll("video"))
+    );
+
+  videos
+    .filter((video) => video.readyState != 0)
+    .map((video) => {
+      video.disablePictureInPicture = false;
+    })
     .sort((v1, v2) => {
-      const v1Rect = v1.getClientRects()[0]||{width:0,height:0};
-      const v2Rect = v2.getClientRects()[0]||{width:0,height:0};
-      return ((v2Rect.width * v2Rect.height) - (v1Rect.width * v1Rect.height));
+      const v1Rect = v1.getClientRects()[0] || { width: 0, height: 0 };
+      const v2Rect = v2.getClientRects()[0] || { width: 0, height: 0 };
+      return v2Rect.width * v2Rect.height - v1Rect.width * v1Rect.height;
     });
 
   if (videos.length === 0) {
@@ -31,21 +29,25 @@ function findLargestPlayingVideo() {
 
 async function requestPictureInPicture(video) {
   await video.requestPictureInPicture();
-  video.setAttribute('__pip__', true);
-  video.addEventListener('leavepictureinpicture', event => {
-    video.removeAttribute('__pip__');
-  }, { once: true });
+  video.setAttribute("__pip__", true);
+  video.addEventListener(
+    "leavepictureinpicture",
+    (event) => {
+      video.removeAttribute("__pip__");
+    },
+    { once: true }
+  );
   new ResizeObserver(maybeUpdatePictureInPictureVideo).observe(video);
 }
 
 function maybeUpdatePictureInPictureVideo(entries, observer) {
   const observedVideo = entries[0].target;
-  if (!document.querySelector('[__pip__]')) {
+  if (!document.querySelector("[__pip__]")) {
     observer.unobserve(observedVideo);
     return;
   }
   const video = findLargestPlayingVideo();
-  if (video && !video.hasAttribute('__pip__')) {
+  if (video && !video.hasAttribute("__pip__")) {
     observer.unobserve(observedVideo);
     requestPictureInPicture(video);
   }
@@ -56,14 +58,14 @@ function maybeUpdatePictureInPictureVideo(entries, observer) {
   if (!video) {
     return;
   }
-  if (video.hasAttribute('__pip__')) {
+  if (video.hasAttribute("__pip__")) {
     document.exitPictureInPicture();
     return;
   }
   await requestPictureInPicture(video);
-  _gaq.push(['_trackPageview', '/']);
+  _gaq.push(["_trackPageview", "/"]);
 })();
 
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-134864766-1']);
-_gaq.push(['_setDetectTitle', false]);
+_gaq.push(["_setAccount", "UA-134864766-1"]);
+_gaq.push(["_setDetectTitle", false]);
